36f4a50cb7cb43b209ffdfdf19b191ac240a88d4
wasquith <william.asquith@ttu.edu>
2019-05-11 14:12:24 -0500

    Update MyPackagePopularity.R

--- MyPackagePopularity.R
+++ MyPackagePopularity.R
@@ -1,8 +1,8 @@
 
 options(repos = c(CRAN = "http://cran.rstudio.com"))
   # Here's an easy way to get all the URLs in R
-  start <- as.Date('2019-01-28'); #start <- as.Date('2013-01-01')
-  today <- as.Date('2019-02-01')
+  start <- as.Date('2019-05-11'); #start <- as.Date('2013-01-01')
+  today <- as.Date('2019-05-12')
 
   all_days <- seq(start, today, by = 'day')
 
@@ -72,11 +72,11 @@ for(i in 1:m) {
 AP[,1] <- as.Date(AP[,1])
 Packages <- AP
 save(Packages, file="Packages.RData"); rm(Packages)
-load("Packages_20130101_20190127.RData") # Packages is coming back
+load("Packages_20130101_20190510.RData") # Packages is coming back
 AP <- merge(Packages, AP, all=TRUE)
 
 #Packages <- AP
-#save(Packages, file="Packages_20130101_20190127.RData")
+#save(Packages, file="Packages_20130101_20190510.RData")
 
 library(kernlab)
 yearize <- 365
